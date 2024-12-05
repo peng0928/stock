cf3fdb63ca16418189c95e2d12347eed773f8129
chenxuepeng <1944542244@qq.com>
2024-12-05 22:46:10 +0800

    更新

--- stock/src/router/index.js
+++ stock/src/router/index.js
@@ -4,12 +4,21 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        component: () => import('../components/HelloWorld.vue')
+        component: () => import('../components/HelloWorld.vue'),
+        meta: {
+            title: '大盘'
+        },
     }
 ];
 
 const router = createRouter({
     history: createWebHistory(''),
     routes
+});
+router.beforeEach((to, from, next) => {
+    if (to.meta.title) {
+        document.title = to.meta.title;
+    }
+    next();
 });
 export default router;
