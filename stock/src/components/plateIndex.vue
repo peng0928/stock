119c3d3d387fb4aafa27b847be95888a08a5b682
chenxuepeng <1944542244@qq.com>
2024-12-08 22:37:52 +0800

    更新

--- stock/src/components/plateIndex.vue
+++ stock/src/components/plateIndex.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
-
+  <div class="flex">
+    <div v-for="(item, index) in plateData" :key="index" class="text-sm w-1/6">
+      <div>{{ item.name }}</div>
+    </div>
   </div>
+
 </template>
 
+<script setup>
+
+import {ref} from "@vue/reactivity";
+
+const plateData = ref([
+  {name: "文化传媒"},
+  {name: "酿酒行业"},
+  {name: "医疗服务"},
+  {name: "游戏"},
+  {name: "保险"},
+  {name: "文化传媒"},
+  {name: "酿酒行业"},
+  {name: "医疗服务"},
+  {name: "游戏"},
+  {name: "保险"},
+  {name: "文化传媒"},
+  {name: "酿酒行业"},
+  {name: "医疗服务"},
+  {name: "游戏"},
+  {name: "保险"},
+])
+</script>
