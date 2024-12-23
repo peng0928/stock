eb1c675432d3c559aae6c72cdab5865e792f2832
penr <oncwnuC_zUIq8GM0n9XSh7B_Le_Q@git.weixin.qq.com>
2024-12-23 23:07:41 +0800

    更新

--- vue-vben-admin/apps/web-antd/src/views/dashboard/bankuan/index.vue
+++ vue-vben-admin/apps/web-antd/src/views/dashboard/bankuan/index.vue
@@ -1,46 +1,69 @@
 <template>
-
-  <div class="card-box w-full px-4 pb-5 pt-3">
-  <div class="grid grid-cols-5 gap-4 text-center pb-8">
-    <div v-for="(item, index) in plateData" :key="index" class="text-sm ">
-      <div class="rounded-lg h-28 flex text-nowrap" :class="chgBgColor(item.zf)">
-        <div class="m-2 text-xs text-left w-1/3">
-          <!--          <div>净额</div>-->
-          <!--          <div>主: {{item.zlje}}</div>-->
-          <!--          <div>超: {{item.jrcddje}}</div>-->
-          <!--          <div>大: {{item.jrddje}}</div>-->
-          <!--          <div>中: {{item.jrzdje}}</div>-->
-          <!--          <div>小: {{item.jrxdje}}</div>-->
+  <div class="card-box w-full p-5">
+    <div class="p-1 flex gap-2">
+      <Button @click="sortByADesc(1)"> 降序</Button>
+      <Button @click="sortByADesc()"> 升序</Button>
+    </div>
+    <div class="flex flex-wrap p-0">
+      <div v-for="(item, index) in plateData" :key="index"
+           class="text-md hover:shadow-xl  border-border group w-1/3 cursor-pointer border-b border-r border-t py-3">
+        <div class="rounded-lg">
+          <div class="pl-3 truncate flex gap-4 text-lg">
+            <div class="text-blue-500">{{ item.name }}</div>
+            <div class="" :class="textColor(item.zf)">{{ item.zf }}%</div>
+          </div>
         </div>
-        <div class="m-4 w-1/3">
-          <div class="text-lg">{{ item.name }}</div>
-          <div class="pt-3 text-lg" :class="textColor(item.zf)">涨幅: {{ item.zf }}</div>
-        </div>
-        <div class="m-2 text-xs w-1/3 text-right">
-          <!--          <div>净占比</div>-->
-          <!--          <div>{{item.zljzb}}</div>-->
-          <!--          <div>{{item.jrcddjzb}}</div>-->
-          <!--          <div>{{item.jrddjzb}}</div>-->
-          <!--          <div>{{item.jrzdjzb}}</div>-->
-          <!--          <div>{{item.jrxdjzb}}</div>-->
+        <div class="pt-5 pb-5">
+          <div class="table-responsive">
+            <div class="overflow-x-auto">
+              <table class="table-auto w-full sm:flex-col md:table">
+                <thead>
+                <tr>
+                  <th></th>
+                  <th>主</th>
+                  <th>超</th>
+                  <th>大</th>
+                  <th>中</th>
+                  <th>小</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr class="text-center">
+                  <td>净额</td>
+                  <td>{{ item.zlje }}</td>
+                  <td>{{ item.jrcddje }}</td>
+                  <td>{{ item.jrddje }}</td>
+                  <td>{{ item.jrzdje }}</td>
+                  <td>{{ item.jrxdje }}</td>
+                </tr>
+                <tr class="text-center">
+                  <td>占比</td>
+                  <td>{{ item.zljzb }}</td>
+                  <td>{{ item.jrcddjzb }}</td>
+                  <td>{{ item.jrddjzb }}</td>
+                  <td>{{ item.jrzdjzb }}</td>
+                  <td>{{ item.jrxdjzb }}</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
-
-  </div>
 </template>
 <script lang="ts" setup>
-
+import {Button, Card, message, notification, Space} from 'ant-design-vue';
 import {onMounted, ref} from "vue";
-
+import {EllipsisText} from '@vben/common-ui';
 
 const plateData = ref([])
 const textColor = (chg = 0) => {
   return chg < 0 ? 'text-green-500 hover:text-green-600' : 'text-red-500 hover:text-red-600';
 };
 const chgBgColor = (chg = 0) => {
-  if (chg === 0){
+  if (chg === 0) {
     return 'bg-blue-200 hover:bg-blue-300'
   }
   return chg < 0 ? 'bg-green-200 hover:bg-green-300' : ' bg-red-200 hover:bg-red-300';
@@ -61,6 +84,14 @@ const StockPlateData = async () => {
     console.log('There was an error!', error);
   }
 };
+
+function sortByADesc(e = 0) {
+  if (e === 0) {
+    plateData.value = plateData.value.sort((a, b) => a.zf - b.zf);
+  } else {
+    plateData.value = plateData.value.sort((a, b) => b.zf - a.zf);
+  }
+}
 
 onMounted(() => {
   StockPlateData();
